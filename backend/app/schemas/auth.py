c14ce0c99bcf1b2d8c21db1d50a482b67e16bf54
ApparentlyTejas <tejas.malhotra.14@gmail.com>
2026-07-17 18:10:04 +0200

    block disposable/temp email domains on registration

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,11 +1,42 @@
 from datetime import datetime
 
-from pydantic import BaseModel, ConfigDict, EmailStr, Field
+from pydantic import BaseModel, ConfigDict, EmailStr, Field, field_validator
+
+# Common disposable/temporary email domains
+_DISPOSABLE_DOMAINS = {
+    "mailinator.com", "guerrillamail.com", "guerrillamail.net", "guerrillamail.org",
+    "guerrillamail.biz", "guerrillamail.de", "guerrillamail.info", "sharklasers.com",
+    "guerrillamailblock.com", "grr.la", "spam4.me", "trashmail.com", "trashmail.me",
+    "trashmail.net", "trashmail.at", "trashmail.io", "trashmail.org", "trashmail.xyz",
+    "tempmail.com", "tempmail.net", "tempmail.org", "temp-mail.org", "temp-mail.io",
+    "throwam.com", "throwaway.email", "dispostable.com", "mailnull.com", "spamgourmet.com",
+    "yopmail.com", "yopmail.fr", "yopmail.net", "spam.la", "spamfree24.org",
+    "fakeinbox.com", "mailnesia.com", "mailnull.com", "maildrop.cc", "spamgourmet.net",
+    "10minutemail.com", "10minutemail.net", "10minutemail.org", "10minutemail.co.uk",
+    "20minutemail.com", "sharklasers.com", "mailnull.com", "spamevader.com",
+    "discard.email", "discardmail.com", "discardmail.de", "spamherelots.com",
+    "mailexpire.com", "spamhereplease.com", "getairmail.com", "filzmail.com",
+    "spamgob.com", "throwam.com", "crap.handcrafted.jp", "hatespam.org",
+    "jetable.fr.nf", "jetable.net", "jetable.org", "noref.in", "zetmail.com",
+    "mt2015.com", "mt2014.com", "mt2009.com", "temporarymail.net", "spambox.us",
+    "privacy.net", "mailbolt.com", "tempinbox.com", "tempr.email", "dispostable.com",
+    "spamgourmet.org", "wegwerfmail.de", "wegwerfmail.net", "wegwerfmail.org",
+    "spambox.info", "spambox.me", "spambox.org", "spambox.net", "spambox.com",
+    "gufum.com", "tmail.com", "cmail.net", "emlhub.com", "eml.pp.ua",
+}
 
 
 class UserRegister(BaseModel):
     email: EmailStr
     password: str = Field(min_length=8, max_length=128)
+
+    @field_validator("email")
+    @classmethod
+    def reject_disposable_email(cls, v: str) -> str:
+        domain = v.split("@")[-1].lower()
+        if domain in _DISPOSABLE_DOMAINS:
+            raise ValueError("Disposable email addresses are not allowed.")
+        return v
 
 
 class UserLogin(BaseModel):
